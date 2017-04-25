8da686d6c432530d31accf611c10c040a7602bf2
Tim Acker <tjacker31@gmail.com>
2017-04-24 21:13:07 -0400

    jQuery and AJAX
    Linked to jQuery library and starting building out AJAX request to
    Spotify API.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 /*jslint esversion: 6, browser: true*/
-/*global window, $, jQuery, alert*/
+/*global window, console, $, jQuery, alert*/
 
+$.ajax({
+  url: 'https://api.spotify.com/v1/search',
+  data: {
+    q: 'roadhouse blues',
+    type: 'album',
+    market: 'US'
+  },
+  success: (music) => {
+    console.log(music);
+  }
+});
