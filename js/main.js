af9269d043816666c52867cac9f9d94981d3959b
Tim Acker <tjacker31@gmail.com>
2017-04-27 00:54:41 -0400

    Search Input Section
    Added search input field and button. Wrapped AJAX request within the
    search button click event and included artist name in card container.
    Also, added a search sass file.

--- js/main.js
+++ js/main.js
@@ -2,47 +2,63 @@
 /*global window, console, $, jQuery, alert*/
 
 const $cardContainer =  $('.card-container');
-let searchForArtist = 'the who';
+const $searchField = $('#search');
+const $searchBtn = $('#btn-search');
 
-// AJAX request to download data from Spotify
-$.ajax({
-  url: 'https://api.spotify.com/v1/search',
-  data: {
-    q: `artist:"${searchForArtist}"`,
-    type: 'album',
-    market: 'US',
-    limit: 24
-  },
-  beforeSend: function(jqXHR, settings) {
-    console.log(settings.url);
-  },
-  success: (results) => {
-    console.log(results);
-    // Iterate over JSON search results to create a card that holds the album cover and title for each array item
-    $.each(results.albums.items, (i, album) => {
-      let coverUrl = '';
-      let albumName = album.name;
-      let albumId = album.id;
-      // Target image with a height of less than or equal to 300px
-      $.each(album.images, (i, image) => {
-        if (image.height <= 300) {
-          coverUrl = image.url;
-          return false;
-        }
-      });// end each image iterator
-      // Call function to generate HTML for the cards and append to the card container ul
-      $cardContainer.append(cardHtml(coverUrl, albumName, albumId));
-    }); // end each album iterator
-  } // end success callback
-}); // end AJAX request
+// Click function to submit search term to Spotify API via AJAX request
+$searchBtn.on('click', function () {
+  let searchValue = $searchField.val();
+  $cardContainer.empty();
+  $searchField.val('');
+  // AJAX request to retrieve data from Spotify
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search',
+    data: {
+      q: `artist:"${searchValue}"`,
+      type: 'album',
+      market: 'US',
+      limit: 24
+    },
+//    beforeSend: function(jqXHR, settings) {
+//      console.log(settings.url);
+//    },
+    success: (results) => {
+      console.log(results);
+      // Iterate over JSON search results to create a card that holds the album cover and title for each array item
+      $.each(results.albums.items, (i, album) => {
+        let coverUrl = '';
+        let albumName = album.name;
+        let artistName = '';
+        let albumId = album.id;
+        // Target image with a height of less than or equal to 300px
+        $.each(album.images, (i, image) => {
+          if (image.height <= 300) {
+            coverUrl = image.url;
+            return false;
+          }
+        });// end each image iterator
+        $.each(album.artists, (i, artist) => {
+          if (i <= 2) {
+            artistName += artist.name + ' / ';
+          } else {
+            return false;
+          }
+        });// end each image iterator
+        artistName = artistName.slice(0, -3);
+        // Call function to generate HTML for the cards and append to the card container ul
+        $cardContainer.append(cardHtml(coverUrl, albumName, artistName, albumId));
+      }); // end each album iterator
+    } // end success callback
+  }); // end AJAX request
+});
 
 // Function to generate the card HTML with album image and name, plus insert album ID as a data attribute in the More Info link
-let cardHtml = (coverUrl, albumName, albumId) => {
+let cardHtml = (coverUrl, albumName, artistName, albumId) => {
   // Create the HTML using a template literal
   let html =
       `<li class="card">
         <figure>
-          <div class="image-overlay-container">
+          <div class="image-overlay">
             <img class="card-image"
             src="${coverUrl}"
             alt="${albumName} album cover">
@@ -51,6 +67,7 @@ let cardHtml = (coverUrl, albumName, albumId) => {
             </div>
           </div>
           <figcaption class="card-name">${albumName}</figcaption>
+          <p class="card-artist">${artistName}</p>
         </figure>
       </li>`;
   
@@ -63,13 +80,10 @@ $cardContainer.on('click', '.btn-info', function (e) {
 
   let albumId = $(this).data('id');
   
-  // AJAX request to download detailed album information from Spotify
+  // AJAX request to retrieve detailed album information from Spotify
   $.ajax({
     url: `https://api.spotify.com/v1/albums/${albumId}`,
 
-    beforeSend: function(jqXHR, settings) {
-      console.log(settings.url);
-    },
     success: (results) => {
       console.log(results);
 
