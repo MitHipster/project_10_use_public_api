f3998f7f9366dbfba4d9060fafe3ac6125f034e5
Tim Acker <tjacker31@gmail.com>
2017-04-30 21:49:52 -0400

    Album Tracks
    Added function to create list items out of each album track for
    inclusion in the More Info section. Also, applied initial styling.
    Lastly, made some revisions to the focus and hover extends, but the
    styling applied remains the same.

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ $searchBtn.on('click', function () {
   }); // end AJAX request
 });
 
-// Iterate over search results to create a card that holds the album information for each object item
+// Iterate over search results to create a card that holds the album information for each object
 let albumResults = (albums) => {
   let artistName = '';
   
@@ -57,12 +57,24 @@ let albumResults = (albums) => {
     // Get medium-sized image with height and width ~300px
     let coverUrl = album.images[1].url;
     let albumName = album.name;
+    // Call function to create artist name(s)
     artistName = artistList(album.artists);
 
     // Call function to generate HTML for the cards and append to the card container ul
     $cardContainer.append(cardHtml(i, coverUrl, albumName, artistName));
   }); // end each album iterator
 }; // end albumResults function
+
+// Build a comma-separated list of the album IDs for subsequent AJAX request
+let albumIdList = (albums) => {
+  let albumList = '';
+  
+  $.each(albums, (i, album) => {
+    albumList += (album.id + ',');
+  }); // end each album iterator
+  // Remove last comma and return list
+  return albumList.slice(0, -1);
+}; // end albumIdList function
 
 // Combine multiple artist names into a string 
 let artistList = (artists) => {
@@ -75,16 +87,20 @@ let artistList = (artists) => {
   return artistNames.slice(0, -3);
 }; // end artistList function
 
-// Build a comma-separated list of the album IDs for subsequent AJAX request
-let albumIdList = (albums) => {
-  let albumList = '';
-  
-  $.each(albums, (i, album) => {
-    albumList += (album.id + ',');
-  }); // end each album iterator
-  // Remove last comma and return list
-  return albumList.slice(0, -1);
-}; // end albumIdList function
+// Iterate over album tracks to create a list for the More Info area
+let albumTracks = (tracks) => {
+  let html = '';
+  $.each(tracks, (i, track) => {
+    let trackPreview = track.preview_url;
+    let trackNum = track.track_number;
+    let trackName = track.name;
+    let trackDuration = msConvert(track.duration_ms);
+
+    // Call function to generate HTML for album tracks
+    html += trackHtml(trackPreview, trackNum , trackName, trackDuration);
+  }); // end each track iterator
+  return html;
+}; // end albumTracks function
 
 // Function to generate the card HTML with album image and name, plus insert album ID as a data attribute in the More Info link
 let cardHtml = (i, coverUrl, albumName, artistName) => {
@@ -108,6 +124,22 @@ let cardHtml = (i, coverUrl, albumName, artistName) => {
   return html;
 }; // end cardHtml function
 
+// Function to generate the track HTML with preview link, name, number and duration
+let trackHtml = (trackPreview, trackNum , trackName, trackDuration) => {
+  // Create the HTML for album track using a template literal
+  let html =
+      `<li class="lg-track">
+        <audio controls class="lg-track-sample mejs__custom"
+          src="${trackPreview}">
+        </audio>
+        <p class="lg-track-num">${trackNum}</p>
+        <p class="lg-track-name">${trackName}</p>
+        <p class="lg-track-duration">${trackDuration}</p>
+      </li>`;
+  
+  return html;
+}; // end trackHtml function
+
 // Click function on More Info button collection to call LightGallery lightbox plugin
 $cardContainer.on('click', '.btn-info', function (e) {
   e.preventDefault();
@@ -123,7 +155,7 @@ $cardContainer.on('click', '.btn-info', function (e) {
     mode: 'lg-fade',
     speed: 200,
     width: '640px',
-    height: '960px',
+//    height: '960px',
     addClass: 'lg-custom',
     getCaptionFromTitleOrAlt: false,
     download: false
@@ -144,31 +176,43 @@ $cardContainer.on('click', '.btn-info', function (e) {
 let albumObjArray = (albums) => {
   let obj = {};
   let artistName = '';
+  let trackList = '';
   // Clear array of earlier search results
   lgDynamicEl = [];
   $.each(albums, (i, album) => {
     let coverUrl = album.images[0].url;
     let albumRelease = album.release_date.slice(0, 4);
+    // Call function to create artist name(s)
     artistName = artistList(album.artists);
+    // Call function to create album tracks
+    trackList = albumTracks(album.tracks.items);
     obj = {
       'src': coverUrl,
       'subHtml': 
-        `<div class="lg-details-container">
-          <p class="lg-album"><span>Album Title: </span>${album.name}</p>
-          <p class="lg-artist"><span>Artist: </span>${artistName}</p>
-          <p class="lg-label"><span>Label: </span>${album.label}</p>
-          <p class="lg-release-date"><span>Released: </span>${albumRelease} (${album.album_type})</p>
-          <div class="flexy">
-            <audio controls class="lg-track-sample mejs__custom"
-              src="https://p.scdn.co/mp3-preview/2d75ef505223315f19fb95d95f9cb1f02398fdc2?cid=null">
-            </audio>
-            <p>Test to see if it fits</p>
-            <p>Test to see if it fits</p>
+        `<div class="lg-album-details">
+          <div class="lg-album-info">
+            <p class="lg-album"><span>Album Title: </span>${album.name}</p>
+            <p class="lg-artist"><span>Artist: </span>${artistName}</p>
+            <p class="lg-label"><span>Label: </span>${album.label}</p>
+            <p class="lg-release-date"><span>Released: </span>${albumRelease} (${album.album_type})</p>
           </div>
+          <ul class="lg-track-info">
+            ${trackList}
+          </ul>
         </div>`
     };
     lgDynamicEl.push(obj);
   }); // end each album iterator
 }; // end albumObjArray function
 
+
+// Function to convert track duration from ms to minutes:seconds
+let msConvert = (duration) => {
+  let seconds = parseInt((duration / 1000) % 60);
+  let minutes = parseInt((duration / (1000 * 60)) % 60);
+
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+  return minutes + ":" + seconds;
+}; // end msConvert function
 
