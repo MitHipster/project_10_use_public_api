16c4c33f6333d520b3e609fc6f6a83c5df870238
Tim Acker <tjacker31@gmail.com>
2017-05-02 17:21:41 -0400

    Re-ordered Javascript
    Re-ordered javascript code to improve flow.

--- js/main.js
+++ js/main.js
@@ -10,40 +10,6 @@ const $sortInput = $('.sort-selection');
 let searchResults = [];
 let lgDynamicEl = []; // Dynamic LightGallery dataset
 
-// Change function to capture sort request
-$sortInput.on('click', function () {
-  let sortOrder = $(this).data('sort');
-  // Remove active class if applicable and add back class on clicked link
-  $sortInput.removeClass('active');
-  $(this).addClass('active');
-  // Clear any previous results
-  $cardContainer.empty();
-  lgDynamicEl = [];
-  // Call search function and pass sort order option
-  searchResults.sort(sortYearReleased(sortOrder));
-  // Call function to create album cards
-  albumResults(searchResults);
-  // Call function to create album detail for LightGallery
-  albumObjArray(searchResults);
-});
-
-// Function to sort albums by year released
-let sortYearReleased = (sort) => {
-  return function (a, b) {
-    let aYear = a.release_date;
-    let bYear = b.release_date;
-    // Include month in sort if provided else default month to 12
-    aYear = (aYear.length === 4) ? aYear + "12" : aYear.slice(0, 7).replace('-', '');
-    bYear = (bYear.length === 4) ? bYear + "12" : bYear.slice(0, 7).replace('-', '');
-    // If sort option is 'asc' sort ascending else default to descending
-    if (sort === 'asc') {
-      return ((aYear < bYear) ? -1 : ((aYear > bYear) ? 1 : 0));
-    } else {
-      return ((aYear < bYear) ? 1 : ((aYear > bYear) ? -1 : 0));
-    }
-  };
-};
-
 // Click function to submit search term to Spotify API via AJAX request
 $searchBtn.on('click', function () {
   let searchValue = $searchField.val();
@@ -83,7 +49,7 @@ $searchBtn.on('click', function () {
             albumResults(results.albums);
             // Call function to create album detail for LightGallery
             albumObjArray(results.albums);
-            // Slide down sort options after a short delay
+            // Show sort options and used search term
             $sortContainer.show();
           } // end success callback function
         }); // end AJAX request
@@ -96,99 +62,24 @@ $searchBtn.on('click', function () {
   }); // end AJAX request
 });
 
-// Iterate over search results to create a card that holds the album information for each object
-let albumResults = (albums) => {
-  let artistName = '';
-  
-  $.each(albums, (i, album) => {
-    // Get medium-sized image with height and width ~300px
-    let coverUrl = album.images[1].url;
-    let albumName = album.name;
-    let releaseDate = album.release_date.slice(0, 4);
-    // Call function to create artist name(s)
-    artistName = artistList(album.artists);
+// Click function to capture sort request
+$sortInput.on('click', function () {
+  let sortOrder = $(this).data('sort');
+  // Remove active class if applicable and add back class on clicked link
+  $sortInput.removeClass('active');
+  $(this).addClass('active');
+  // Clear any previous results
+  $cardContainer.empty();
+  lgDynamicEl = [];
+  // Call search function and pass sort order option
+  searchResults.sort(sortYearReleased(sortOrder));
+  // Call function to create album cards
+  albumResults(searchResults);
+  // Call function to create album detail for LightGallery
+  albumObjArray(searchResults);
+});
 
-    // Call function to generate HTML for the cards and append to the card container ul
-    $cardContainer.append(cardHtml(i, coverUrl, albumName, artistName, releaseDate));
-  }); // end each album iterator
-}; // end albumResults function
-
-// Build a comma-separated list of the album IDs for subsequent AJAX request
-let albumIdList = (albums) => {
-  let albumList = '';
-  
-  $.each(albums, (i, album) => {
-    albumList += (album.id + ',');
-  }); // end each album iterator
-  // Remove last comma and return list
-  return albumList.slice(0, -1);
-}; // end albumIdList function
-
-// Combine multiple artist names into a string 
-let artistList = (artists) => {
-  let artistNames = '';
-
-  $.each(artists, (i, artist) => {
-    artistNames += artist.name + ' / ';
-  }); // end each artist iterator
-  // Remove last set of " / " and return list
-  return artistNames.slice(0, -3);
-}; // end artistList function
-
-// Iterate over album tracks to create a list for the More Info area
-let albumTracks = (tracks) => {
-  let html = '';
-  $.each(tracks, (i, track) => {
-    let trackPreview = track.preview_url;
-    let trackNum = track.track_number;
-    let trackName = track.name;
-    let trackDuration = msConvert(track.duration_ms);
-
-    // Call function to generate HTML for album tracks
-    html += trackHtml(trackPreview, trackNum , trackName, trackDuration);
-  }); // end each track iterator
-  return html;
-}; // end albumTracks function
-
-// Function to generate the card HTML with album image and name, plus insert album ID as a data attribute in the More Info link
-let cardHtml = (i, coverUrl, albumName, artistName, releaseDate) => {
-  // Create the HTML using a template literal
-  let html =
-      `<li class="card">
-        <figure>
-          <div class="image-overlay">
-            <img class="card-image"
-            src="${coverUrl}"
-            alt="${albumName} album cover">
-            <div class="btn-overlay">
-              <a href="#0" class="btn-info" data-index="${i}">More Info</a>
-            </div>
-          </div>
-          <figcaption class="card-name">${albumName}</figcaption>
-          <p class="card-artist">${artistName} (${releaseDate})</p>
-        </figure>
-      </li>`;
-  
-  return html;
-}; // end cardHtml function
-
-// Function to generate the track HTML with preview link, name, number and duration
-let trackHtml = (trackPreview, trackNum , trackName, trackDuration) => {
-  // Create the HTML for album track using a template literal
-  let html =
-      `<li class="lg-track">
-        <audio controls class="lg-track-sample mejs__custom"
-          src="${trackPreview}">
-        </audio>
-        <p class="lg-track-num">${trackNum}</p>
-        <p class="lg-track-name">${trackName}</p>
-        <p class="lg-track-duration">${trackDuration}</p>
-      </li>`;
-  
-  return html;
-}; // end trackHtml function
-
-// Click function on More Info button collection to call LightGallery lightbox plugin
+// Click function for More Info button collection to call LightGallery lightbox plugin
 $cardContainer.on('click', '.btn-info', function (e) {
   e.preventDefault();
   // Get index data from button to set starting point for lightbox
@@ -219,6 +110,115 @@ $cardContainer.on('click', '.btn-info', function (e) {
     });
   }); // end LightGallery constructor
 }); // .btn-info click function
+
+// Build a comma-separated list of the album IDs for subsequent AJAX request
+let albumIdList = (albums) => {
+  let albumList = '';
+  
+  $.each(albums, (i, album) => {
+    albumList += (album.id + ',');
+  }); // end each album iterator
+  // Remove last comma and return list
+  return albumList.slice(0, -1);
+}; // end albumIdList function
+
+// Iterate over search results to create a card that holds the album information for each object
+let albumResults = (albums) => {
+  let artistName = '';
+  
+  $.each(albums, (i, album) => {
+    // Get medium-sized image with height and width ~300px
+    let coverUrl = album.images[1].url;
+    let albumName = album.name;
+    let releaseDate = album.release_date.slice(0, 4);
+    // Call function to create artist name(s)
+    artistName = artistList(album.artists);
+
+    // Call function to generate HTML for the cards and append to the card container ul
+    $cardContainer.append(cardHtml(i, coverUrl, albumName, artistName, releaseDate));
+  }); // end each album iterator
+}; // end albumResults function
+
+// Combine multiple artist names into a string 
+let artistList = (artists) => {
+  let artistNames = '';
+
+  $.each(artists, (i, artist) => {
+    artistNames += artist.name + ' / ';
+  }); // end each artist iterator
+  // Remove last set of " / " and return list
+  return artistNames.slice(0, -3);
+}; // end artistList function
+
+// Function to generate the card HTML with album image and name, plus insert album ID as a data attribute in the More Info link
+let cardHtml = (i, coverUrl, albumName, artistName, releaseDate) => {
+  // Create the HTML using a template literal
+  let html =
+      `<li class="card">
+        <figure>
+          <div class="image-overlay">
+            <img class="card-image"
+            src="${coverUrl}"
+            alt="${albumName} album cover">
+            <div class="btn-overlay">
+              <a href="#0" class="btn-info" data-index="${i}">More Info</a>
+            </div>
+          </div>
+          <figcaption class="card-name">${albumName}</figcaption>
+          <p class="card-artist">${artistName} (${releaseDate})</p>
+        </figure>
+      </li>`;
+  
+  return html;
+}; // end cardHtml function
+
+// Function to sort albums by year released
+let sortYearReleased = (sort) => {
+  return function (a, b) {
+    let aYear = a.release_date;
+    let bYear = b.release_date;
+    // Include month in sort if provided else default month to 12
+    aYear = (aYear.length === 4) ? aYear + "12" : aYear.slice(0, 7).replace('-', '');
+    bYear = (bYear.length === 4) ? bYear + "12" : bYear.slice(0, 7).replace('-', '');
+    // If sort option is 'asc' sort ascending else default to descending
+    if (sort === 'asc') {
+      return ((aYear < bYear) ? -1 : ((aYear > bYear) ? 1 : 0));
+    } else {
+      return ((aYear < bYear) ? 1 : ((aYear > bYear) ? -1 : 0));
+    }
+  };
+};
+
+// Iterate over album tracks to create a list for the More Info area
+let albumTracks = (tracks) => {
+  let html = '';
+  $.each(tracks, (i, track) => {
+    let trackPreview = track.preview_url;
+    let trackNum = track.track_number;
+    let trackName = track.name;
+    let trackDuration = msConvert(track.duration_ms);
+
+    // Call function to generate HTML for album tracks
+    html += trackHtml(trackPreview, trackNum , trackName, trackDuration);
+  }); // end each track iterator
+  return html;
+}; // end albumTracks function
+
+// Function to generate the track HTML with preview link, name, number and duration
+let trackHtml = (trackPreview, trackNum , trackName, trackDuration) => {
+  // Create the HTML for album track using a template literal
+  let html =
+      `<li class="lg-track">
+        <audio controls class="lg-track-sample mejs__custom"
+          src="${trackPreview}">
+        </audio>
+        <p class="lg-track-num">${trackNum}</p>
+        <p class="lg-track-name">${trackName}</p>
+        <p class="lg-track-duration">${trackDuration}</p>
+      </li>`;
+  
+  return html;
+}; // end trackHtml function
 
 // Function to create an array of album objects for use as dynamic LightGallery dataset
 let albumObjArray = (albums) => {
@@ -253,7 +253,6 @@ let albumObjArray = (albums) => {
     lgDynamicEl.push(obj);
   }); // end each album iterator
 }; // end albumObjArray function
-
 
 // Function to convert track duration from ms to minutes:seconds
 let msConvert = (duration) => {
